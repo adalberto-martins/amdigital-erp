71371c61338368bc5fd2b083043a09d950a15161
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-12-21 15:23:04 -0300

    Create login.php

---
+++ public/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login - AM Digital ERP</title>
+</head>
+<body>
+
+<h1>Login</h1>
+
+<form method="post" action="login_processa.php">
+    <label>Email</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <label>Senha</label><br>
+    <input type="password" name="senha" required><br><br>
+
+    <button type="submit">Entrar</button>
+</form>
+
+</body>
+</html>
