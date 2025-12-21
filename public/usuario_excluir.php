512df1e3b28975faea3a5d5fbd9088d0827c24e7
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-12-21 16:59:13 -0300

    Update usuario_excluir.php

--- public/usuario_excluir.php
+++ public/usuario_excluir.php
@@ -1,21 +1,16 @@
 <?php
-require "../app/auth/verifica_login.php";
+require __DIR__ . "/../app/auth/seguranca.php";
 
-// Somente admin
-if ($_SESSION['usuario_nivel'] !== 'admin') {
-    header("Location: dashboard.php");
-    exit;
-}
 
-require "../config/database.php";
 
 $id = $_GET['id'] ?? null;
 
 // Não permite excluir a si mesmo
-if (!$id || $id == $_SESSION['usuario_id']) {
+if ($id == $_SESSION['usuario_id']) {
     header("Location: usuarios.php");
     exit;
 }
+
 
 // Excluir usuário
 $sql = "DELETE FROM usuarios WHERE id = ?";
