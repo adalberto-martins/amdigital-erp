e70da841e434a6c0bb7df217a4c073a2dfe30de6
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-12-22 00:58:13 -0300

    Update usuario_excluir.php

--- public/usuario_excluir.php
+++ public/usuario_excluir.php
@@ -1,16 +1,21 @@
 <?php
 require __DIR__ . "/../app/auth/seguranca.php";
+exigeAdmin();
 
+require __DIR__ . "/../config/database.php";
 
+// Garantia defensiva
+if (!isset($pdo)) {
+    die("Erro crítico: conexão com banco não encontrada.");
+}
 
 $id = $_GET['id'] ?? null;
 
 // Não permite excluir a si mesmo
-if ($id == $_SESSION['usuario_id']) {
+if (!$id || $id == $_SESSION['usuario_id']) {
     header("Location: usuarios.php");
     exit;
 }
-
 
 // Excluir usuário
 $sql = "DELETE FROM usuarios WHERE id = ?";
