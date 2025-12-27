3d6693f77eaca3207d368734f11890ffc06064ce
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-12-27 18:12:26 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,22 +1,19 @@
 <?php
+$host = "localhost";
+$banco = "amdigital_erp";
+$usuario = "root";
+$senha = "";
 
-function conexao() {
-    $host = 'localhost';
-    $db   = 'amdigital';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
-
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-
-    // FORÇA UTF-8 REAL
-    $pdo->exec("SET NAMES utf8mb4");
-
-    return $pdo;
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8mb4",
+        $usuario,
+        $senha,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("ERRO BANCO: " . $e->getMessage());
 }
-
